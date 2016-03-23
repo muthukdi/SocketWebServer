81b7b8555160c537dbe82b1e063bcbbb89e050f2
Dilip Muthukrishnan <cheepu82@gmail.com>
2016-03-23 14:45:07 -0400

    Added start and stop scripts for both Windows and Unix

--- start.bat
+++ start.bat
@@ -1,3 +1,10 @@
 @echo off
 
-java base.SocketWebServer
+:: Set the location of your JDK
+set JAVA_HOME="C:\Program Files (x86)\Java\jdk1.7.0_75"
+
+:: Compile all application source files
+for /d %%i in (base\apps\*) do %JAVA_HOME%\bin\javac -sourcepath . -cp %%i\*.jar %%i\*.java
+
+:: Run the web server
+%JAVA_HOME%\bin\java base.SocketWebServer
--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Set the location of Java home
+# Set the location of your JDK
 JAVA_HOME='/home/ec2-user/jdk1.7.0_79'
 
-# Compile web server source files
-$JAVA_HOME/bin/javac base/SocketWebServer.java
+# Compile all application source files
+for i in $(ls -d base/apps/*/); 
+do
+	$JAVA_HOME/bin/javac -sourcepath . -cp ${i}*.jar ${i}*.java
+done
 
 # Run the web server
-$JAVA_HOME/bin/java base.SocketWebServer
+if [ -z "$1" ]
+then
+	$JAVA_HOME/bin/java base.SocketWebServer &
+else
+	$JAVA_HOME/bin/java base.SocketWebServer
+fi
